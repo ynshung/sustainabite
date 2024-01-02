dcd38491531da6d1e6d60d497939ca67b7a54c10
Young Shung <me@ynshung.com>
2024-01-03 03:10:02 +0800

    init proj

---
+++ app/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAbsrhVlwxbUXIE9cCR3T_NRjjGBKwD3BU",
+    authDomain: "sustainabite-usm1.firebaseapp.com",
+    projectId: "sustainabite-usm1",
+    storageBucket: "sustainabite-usm1.appspot.com",
+    messagingSenderId: "735167693032",
+    appId: "1:735167693032:web:7ba5b6c97403c671e7095d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
